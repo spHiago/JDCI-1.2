2d36e43d76c3a4f88d1d9e9549e918a40564d3cd
spHiago <hiagofranco2604@gmail.com>
2025-05-19 20:55:12 +0100

    Add files via upload

---
+++ ex05_ExecuteAplication/ex05_ExecuteAplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ex05_ExecuteAplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
