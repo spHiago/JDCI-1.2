ca1d1293241bdb3741a802952783da62da7f9d2e
Hiago Franco <hiago.franco@students.assembly.pt>
2025-05-19 18:03:33 +0100

    Enunciado

---
+++ ex04_BizzBuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        /* . The application receives two number, calculates the multiplication and return the word
+“Bizz” when even (par) and “Buzz” when odd (impar); */
+
+        Console.WriteLine("Hello, i will take two numbers, \nmultiply them, and tell you if the result is even.");
+
+}
